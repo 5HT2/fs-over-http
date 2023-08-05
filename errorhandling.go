1ee01b22f9288ac94294cbe7694a744d0133370e
5HT2 <git@frogg.ie>
2023-08-05 16:58:12 +0200

    impr: Use realip for logging

--- errorhandling.go
+++ errorhandling.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/valyala/fasthttp"
 	"log"
 	"strings"
+
+	"github.com/ferluci/fast-realip"
+	"github.com/valyala/fasthttp"
 )
 
 func HandleModifyFsFolder(ctx *fasthttp.RequestCtx) {
@@ -23,18 +25,18 @@ func HandleForbidden(ctx *fasthttp.RequestCtx) {
 	ctx.Response.Header.Set("X-Server-Message", "403 Forbidden")
 	fmt.Fprint(ctx, "403 Forbidden\n")
 	log.Printf("- Returned 403 to %s - tried to connect with '%s' to '%s'",
-		ctx.RemoteIP(), ctx.Request.Header.Peek("Auth"), ctx.Path())
+		realip.FromRequest(ctx), ctx.Request.Header.Peek("Auth"), ctx.Path())
 }
 
 func HandleInternalServerError(ctx *fasthttp.RequestCtx, err error) {
 	if strings.HasSuffix(err.Error(), "no such file or directory") {
 		HandleGeneric(ctx, fasthttp.StatusNotFound, "Not Found")
-		log.Printf("- Returned 404 to %s with error %v", ctx.RemoteIP(), err)
+		log.Printf("- Returned 404 to %s with error %v", realip.FromRequest(ctx), err)
 		return
 	}
 
 	ctx.Response.SetStatusCode(fasthttp.StatusInternalServerError)
 	ctx.Response.Header.Set("X-Server-Message", "500 "+err.Error())
 	fmt.Fprintf(ctx, "500 %v\n", err)
-	log.Printf("- Returned 500 to %s with error %v", ctx.RemoteIP(), err)
+	log.Printf("- Returned 500 to %s with error %v", realip.FromRequest(ctx), err)
 }
